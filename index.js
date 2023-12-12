7d50befb32f506bc3a151451f7ac29f42c246971
Victor Muhoro <victormuhoro7@gmail.com>
2023-12-12 23:06:41 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide() {
+    return 2000/100;
+}
+
+let square = (parameter1) => {
+    return parameter1 * parameter1;
+
+}
+
+let add = (parameter2, parameter3) => {
+    return parameter2 + parameter3;
+    
+}
